e3df870919cc9c17e6417e1fedd3d4cf13a85785
DharmendraSingh-97 <dharmendrasinghbhopal09@gmail.com>
2024-10-08 10:58:34 +0530

    first push

---
+++ SeleniumProjectCMD/src/test/java/pac2/createOrgTest.java
@@ -0,0 +1,18 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class createOrgTest  
+	{
+		@Test
+		public void createOrgTest1()
+		{
+			System.out.println("execute createOrgTest");
+		}
+		
+		@Test
+		public void modifyOrgTest()
+		{
+			System.out.println("execute modifyOrgTest");
+		}
+}
